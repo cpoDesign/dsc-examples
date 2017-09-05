76a0ffa4319b2708e51b1fbeeb08da3327ec15a3
Pavel <cpoDesign@users.noreply.github.com>
2017-09-05 21:44:15 +0100

    Create dsc-create-file-share.ps1

---
+++ dsc-create-file-share.ps1
@@ -0,0 +1,9 @@
+# source for package: https://www.powershellgallery.com/packages/xSmbShare/
+
+xSmbShare ScriptingShare
+{
+  Ensure = "present"
+  Name   = "Share"
+  Path = "C:\Data" 
+  Description = "This is a scripting SMB Share"         
+}
